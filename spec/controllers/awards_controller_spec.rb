e4be7d5f86a15daa4eaa2e6fef08f110ea1e55cc
Ivan Grishkov <grishkov.i.a@gmail.com>
2018-08-27 04:41:23 +0700

    repo_awards tests

--- spec/controllers/awards_controller_spec.rb
+++ spec/controllers/awards_controller_spec.rb
@@ -1,10 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe AwardsController, type: :controller do
-  describe 'GET#index' do
+  feature 'GET#index' do
     it 'render index view' do
       get :index
       expect(response).to render_template :index
     end
   end
+
+  feature 'GET#repo_awards' do
+    it 'should validate api_url'
+    it 'should redirect back if api_url invalid'
+    it 'should get api response'
+    it 'should redirect back if response invalid'
+    it 'should get top 3 contributors'
+
+    it 'render repo_awards view' do
+      get :repo_awards
+      expect(response).to render_template :repo_awards
+    end
+  end
+
+
 end
