41ed53eb8eef91e5dff157d26e45f02550d20f25
Ivan Grishkov <grishkov.i.a@gmail.com>
2018-08-27 04:10:19 +0700

    test controller index

--- spec/controllers/awards_controller_spec.rb
+++ spec/controllers/awards_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe AwardsController, type: :controller do
-
+  describe 'GET#index' do
+    it 'render index view' do
+      get :index
+      expect(response).to render_template :index
+    end
+  end
 end
